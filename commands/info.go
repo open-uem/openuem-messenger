55f07825edb9b36142d7c898490d58e490b8eac5
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-26 18:25:36 +0100

    feat: replace zenity windows messages with html files

--- commands/info.go
+++ commands/info.go
@@ -1,12 +1,18 @@
 package commands
 
 import (
+	"html/template"
+	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/ncruces/zenity"
+	"github.com/pkg/browser"
 	"github.com/urfave/cli/v2"
 )
+
+type Message struct {
+	PIN string
+}
 
 func InfoMessage() *cli.Command {
 	return &cli.Command{
@@ -18,27 +24,64 @@ func InfoMessage() *cli.Command {
 }
 
 func showInfoMessage(cCtx *cli.Context) error {
-	ex, err := os.Executable()
+	messageType := cCtx.String("type")
+	message := Message{}
+
+	switch messageType {
+	case "pin":
+		message.PIN = cCtx.String("message")
+		return showPINMessage(&message)
+	}
+
+	return nil
+}
+
+func showPINMessage(m *Message) error {
+	wd, err := GetWd()
 	if err != nil {
 		return err
 	}
 
-	return zenity.Info(cCtx.String("message"),
-		zenity.Title(cCtx.String("title")),
-		zenity.InfoIcon,
-		zenity.WindowIcon(filepath.Join(filepath.Dir(ex), "assets", "icon.png")))
+	templatesPath := filepath.Join(wd, "templates", "pin.templ")
+	tmpl, err := template.New("pin.templ").ParseFiles(templatesPath)
+	if err != nil {
+		log.Printf("[ERROR]: could not open the template: %v", err)
+		return err
+	}
+
+	dstPath := filepath.Join(os.TempDir(), "pin.html")
+	file, err := os.Create(dstPath)
+	if err != nil {
+		log.Printf("[ERROR]: could not create the message file: %v", err)
+		return err
+	}
+	defer os.Remove(dstPath)
+
+	err = tmpl.Execute(file, m)
+	if err != nil {
+		log.Printf("[ERROR]:could not generate the message file from the template: %v", err.Error())
+		return err
+	}
+
+	browser.OpenFile(file.Name())
+
+	return nil
 }
 
 func InfoFlags() []cli.Flag {
 	return []cli.Flag{
 		&cli.StringFlag{
-			Name:     "title",
-			Usage:    "the title you want to show",
-			Required: true,
+			Name:  "title",
+			Usage: "the title you want to show",
 		},
 		&cli.StringFlag{
 			Name:     "message",
 			Usage:    "the message you want to show",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "type",
+			Usage:    "the type of message you want to show",
 			Required: true,
 		},
 	}
--- go.mod
+++ go.mod
@@ -3,19 +3,13 @@ module github.com/doncicuto/openuem_message
 go 1.23.1
 
 require (
-	github.com/ncruces/zenity v0.10.14
+	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/urfave/cli/v2 v2.27.4
 )
 
 require (
-	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
-	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
-	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
-	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	golang.org/x/image v0.20.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
